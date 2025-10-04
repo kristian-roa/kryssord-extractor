324591290f061cb2355d32d814fbe99fcda7b112
prosper-kristian <kristian@prosper-ai.no>
2025-10-04 14:09:24 +0200

    fix upload folder

--- remarkable.py
+++ remarkable.py
@@ -6,7 +6,7 @@ def upload_to_remarkable(filename, is_solution=False):
     folder = REMARKABLE_FOLDER + "/Løsninger" if is_solution else REMARKABLE_FOLDER
 
     try:
-        subprocess.run(["/usr/local/bin/rmapi", "put", filename, REMARKABLE_FOLDER], check=True)
-        print(f"? Uploaded to reMarkable: {REMARKABLE_FOLDER}/{filename}")
+        subprocess.run(["/usr/local/bin/rmapi", "put", filename, folder], check=True)
+        print(f"? Uploaded to reMarkable: {folder}/{filename}")
     except subprocess.CalledProcessError as e:
         print(f"? Failed to upload to reMarkable: {e}")
