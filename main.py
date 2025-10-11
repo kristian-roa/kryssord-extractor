9eb97c2c2e84fa862114e98967addc3d509e99fc
prosper-kristian <kristian@prosper-ai.no>
2025-10-11 15:47:40 +0200

    add optional upload

--- main.py
+++ main.py
@@ -4,27 +4,29 @@
 import argparse
 import kryssord_no
 import gratis_kryssord
+import remarkable
 
 
-def download_and_upload_kryssord_no():
+def download_and_upload_kryssord_no(upload):
     kryssord_path, solution_path = kryssord_no.download_crossword()
-    # remarkable.upload_to_remarkable(kryssord_path)
-    # remarkable.upload_to_remarkable(solution_path, True)
+    if upload: remarkable.upload_to_remarkable(kryssord_path)
+    if upload: remarkable.upload_to_remarkable(solution_path, True)
 
 
-def download_and_upload_gratis_kryssord():
+def download_and_upload_gratis_kryssord(upload):
     kryssord_path = gratis_kryssord.download_gratiskryssord("https://www.gratiskryssord.no/kryssord/dagens/")
-    # remarkable.upload_to_remarkable(kryssord_path)
+    if upload: remarkable.upload_to_remarkable(kryssord_path)
 
 
 def main():
     ap = argparse.ArgumentParser(description="Save crossword + solution as trimmed PDFs with date-based filenames.")
     ap.add_argument("--kryssord-no", default=False)
     ap.add_argument("--gratis-kryssord", default=True)
+    ap.add_argument("--upload", default=False)
     args = ap.parse_args()
 
-    if args.kryssord_no: download_and_upload_kryssord_no()
-    if args.gratis_kryssord: download_and_upload_gratis_kryssord()
+    if args.kryssord_no: download_and_upload_kryssord_no(args.upload)
+    if args.gratis_kryssord: download_and_upload_gratis_kryssord(args.upload)
 
 
 if __name__ == "__main__":
