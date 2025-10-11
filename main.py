2bccbfaed3844251ca0e8d470cfad522a64554f4
prosper-kristian <kristian@prosper-ai.no>
2025-10-11 14:17:23 +0200

    refactor main

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+import argparse
 import kryssord_no
 import gratis_kryssord
 import remarkable
 
 
-def main():
+def download_and_upload_kryssord_no():
     kryssord_path, solution_path = kryssord_no.download_crossword()
-    # gratis_kryssord.download_gratiskryssord("https://www.gratiskryssord.no/kryssord/dagens/")
     remarkable.upload_to_remarkable(kryssord_path)
     remarkable.upload_to_remarkable(solution_path, True)
+
+
+def download_and_upload_gratis_kryssord():
+    kryssord_path = gratis_kryssord.download_gratiskryssord("https://www.gratiskryssord.no/kryssord/dagens/")
+    remarkable.upload_to_remarkable(kryssord_path)
+
+
+def main():
+    ap = argparse.ArgumentParser(description="Save crossword + solution as trimmed PDFs with date-based filenames.")
+    ap.add_argument("--kryssord-no", default=False)
+    ap.add_argument("--gratis-kryssord", default=True)
+    args = ap.parse_args()
+
+    if args.kryssord_no: download_and_upload_kryssord_no()
+    if args.gratis_kryssord: download_and_upload_gratis_kryssord()
 
 
 if __name__ == "__main__":
